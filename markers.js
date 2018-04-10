7815d7214f1ea947c494ccca603986ed2d86f76c
thijsfenne <thijsfenne@gmail.com>
2018-04-10 23:00:39 +0200

    Initial commit

---
+++ markers.js
@@ -0,0 +1,35 @@
+var beerCafes = [
+	{
+		name: 'de Paas',
+		location: {lat: 52.073523005070804,lng: 4.315582827556108},
+		ranking: 4
+	}, {
+		name: 'de Pakschuit',
+		location: {lat: 52.07335463952028, lng: 4.315087624716734},
+		ranking: 4
+	}, {
+		name: 'the Beergarden',
+		location: {lat: 52.07780591224763,lng: 4.30793048523662},
+		ranking: 5
+	}, {
+		name: 'Hupple The Pub',
+		location: {lat: 52.07909125546776, lng: 4.307356461075296},
+		ranking: 3
+	}, {
+		name: 'Hoppzak',
+		location: {lat: 52.07898987792002, lng: 4.308005220380773},
+		ranking: 4
+	}, {
+		name: 'the American Bar',
+		location: {lat: 52.07576280411542, lng: 4.313044116723512},
+		ranking: 1
+	}, {
+		name: 'café Rootz',
+		location: {lat:52.075910966085495, lng: 4.309948847520326},
+		ranking: 3
+	}, {
+		name: 'grand café Xieje',
+		location: {lat:52.0681463281358, lng: 4.32337263308761},
+		ranking: 2
+	}
+];
---
+++ scripts/markers.js
@@ -0,0 +1,9 @@
+var map;
+
+var marker = new google.maps.Marker({
+    position: {lat: 52.379189, lng: 4.899431},
+    map: map,
+    title: 'Marker 1'
+});
+
+
